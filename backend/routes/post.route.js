aea580d8e9b98357de4528c83ee1eb18ffd68111
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2023-06-15 20:38:32 +0530

    day_3_fw22_0878

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -9,11 +9,57 @@ const postRouter = express.Router();
 //add a post
 postRouter.post("/add", async (req, res) => {
   try {
-    const note = new PostModel(req.body);
-    await note.save();
+    const post = new PostModel(req.body);
+    await post.save();
     res.json({ msg: "New post has been added", post: req.body });
   } catch (err) {
     res.json({ error: err.message });
+  }
+});
+
+//get the post
+postRouter.get("/", async (req, res) => {
+  try {
+    const posts = await PostModel.find({ userID: req.body.userID });
+    res.send(posts);
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+//update
+postRouter.patch("/update/:postID", async (req, res) => {
+  const userIDinUserDoc = req.body.userID;
+  const { postID } = req.params;
+  try {
+    const post = await PostModel.findOne({ _id: postID });
+    const userIDinPostDoc = post.userID;
+    if (userIDinUserDoc == userIDinPostDoc) {
+      await PostModel.findByIdAndUpdate({ _id: postID }, req.body);
+      res.json({ msg: `${post.title} has been updated` });
+    } else {
+      res.json({ msg: "Not Authorized!!" });
+    }
+  } catch (err) {
+    res.json({ error: err });
+  }
+});
+
+//delete
+postRouter.delete("/delete/:postID", async (req, res) => {
+  const userIDinUserDoc = req.body.userID;
+  const { postID } = req.params;
+  try {
+    const post = await PostModel.findOne({ _id: postID });
+    const userIDinPostDoc = post.userID;
+    if (userIDinUserDoc == userIDinPostDoc) {
+      await PostModel.findByIdAndDelete({ _id: postID }, req.body);
+      res.json({ msg: `${post.title} has been deleted` });
+    } else {
+      res.json({ msg: "Not Authorized!!" });
+    }
+  } catch (err) {
+    res.json({ error: err });
   }
 });
 
