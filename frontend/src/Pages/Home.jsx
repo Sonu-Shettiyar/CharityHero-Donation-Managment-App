2a3623904cf82239771e5cd7b43aa886a418fb6e
ank501 <ankitsyadav246@gmail.com>
2023-06-17 17:37:40 +0530

    landing page is done with some components

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Guide from '../components/HomePageComponents/Guide'
 import Banner from '../components/HomePageComponents/Banner'
+import Bottom from '../components/HomePageComponents/Bottom'
+import CardBanner from '../components/HomePageComponents/CardBanner'
 
 const Home = () => {
   return (
     <div>
+ 
 
 
         <Guide/>
- <br />
- <br />
-      <Banner/>
+        <br />
+        <Banner/>
+        <br />
+        <CardBanner/>
+        <br />
+        <Bottom/>   
 
     </div>
   )
